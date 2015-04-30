0bf38593c849f8e3179938de48effc221181131a
Eric L. Dattore <eric.dattore@gmail.com>
2015-04-30 11:42:27 -0600

    refactor(async): Added back std::async support Initially removed, I added back std::async into the main.cpp file. If there are issues, just remove it from the main.cpp file to avoid potential segfaults on Linux. -ED

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,11 @@
 #include "../headers/JackTokenizer.hpp"
 #include "../headers/CompilationEngine.hpp"
 
+using FuturesVec = vector<future<void>>;
+
 int main(int argc, char** argv)
 {
+    FuturesVec doNothingCollection;
     if (argc < 2) {
         cerr << "Too few arguments!" << endl;
         return -1;
@@ -11,13 +14,14 @@ int main(int argc, char** argv)
 
     auto files = getFilesInProject(argv[1]);
     for (auto const& file : files) {
-        ifstream inputFile{file.string()};
-        string filename = file.string();
-        filename = filename.substr(0, filename.find_last_of(".")) + ".vm";
-        ofstream outputFile{filename};
-        CompilationEngine jackCompiler(inputFile, outputFile);
-        inputFile.close();
-        outputFile.close();
+        doNothingCollection.emplace_back(async(launch::async, [&](){
+            ifstream inputFile{file.string()};
+            string filename = file.string();
+            filename = filename.substr(0, filename.find_last_of(".")) + ".vm";
+            ofstream outputFile{filename};
+            CompilationEngine jackCompiler(inputFile, outputFile);
+            inputFile.close();
+            outputFile.close();
+        }));
     }
 }
-
